84a16ab2c89c6f974694fcadc0a8618466e64866
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-10-24 22:51:26 -0400

    Support theme() layer.

--- jquery_cycle2.js
+++ jquery_cycle2.js
@@ -60,26 +60,52 @@ function jquery_cycle2_drupalgap_goto_preprocess(path) {
  */
 function theme_jquery_cycle2(variables) {
 
+  // Set any defaults that weren't provided.
+  var defaults = {
+    display: {
+      prefix: '',
+      field_prefix: '',
+      field_suffix: '',
+      suffix: ''
+    }
+  };
+  variables.display = $.extend({}, defaults.display, variables.display);
+  if (variables.settings) {
+    $.extend(true, variables.attributes, variables.settings);
+  }
+  if (variables.attributes.class.indexOf('cycle-slideshow') == -1) {
+    variables.attributes.class += ' cycle-slideshow ';
+  }
+
   // Open the cycle div container.
   var html = variables.display.prefix + '<div ' + drupalgap_attributes(variables.attributes) + '>' +
       variables.display.field_prefix;
 
-  // Iterate over each item and render them.
+  // Iterate over each item and render them. Try to figure out which path to use for the
+  // image. We want to support Drupal core's "uri", Views JSON "src", and DrupalGap's
+  // "path" variables for all to use.
   $.each(variables.items, function (delta, item) {
-    var image = {
-      path: drupalgap_image_path(item.uri)
-    };
-    html += theme('image', image);
+    var path = null;
+    if (item.uri) { path = drupalgap_image_path(item.uri); }
+    else if (item.src) { path = item.src; }
+    else if (item.path) { path = item.path; }
+    if (path) {
+      var image = { path: path };
+      html += theme('image', image);
+    }
   });
 
   // Close the cycle div container.
   html += variables.display.field_suffix + '</div>' + variables.display.suffix;
 
   // Add a papeshow handler to initialize the cycle.
+  var pageshowKey = variables.entity_type && variables.entity ?
+    variables.entity_type + '-' + variables.entity[entity_primary_key(variables.entity_type)] :
+    user_password();
   html += drupalgap_jqm_page_event_script_code({
     jqm_page_event_callback: 'jquery_cycle2_pageshow',
     jqm_page_event_args: JSON.stringify({})
-  }, variables.entity_type + '-' + variables.entity[entity_primary_key(variables.entity_type)]);
+  }, pageshowKey);
 
   return html;
 }
